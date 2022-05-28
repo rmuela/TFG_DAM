46659be9e5b14d0ea79a009995468f7bf4c079e6
Rafael Muela <rafael316muela@gmail.com>
2022-05-28 21:56:26 +0200

    Crear interfaz login

--- WEB/src/app/app-routing.module.ts
+++ WEB/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404ComponentComponent } from './components/error404-component/error404-component.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
   {
     path: '**',
     component: Error404ComponentComponent,
--- WEB/src/app/app.module.ts
+++ WEB/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Error404ComponentComponent } from './components/error404-component/error404-component.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Error404ComponentComponent
+    Error404ComponentComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
