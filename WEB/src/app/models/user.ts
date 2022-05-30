deddfa598b15b478d600cbe7ba67d62de4dd82c7
Rafael Muela <rafael316muela@gmail.com>
2022-05-30 17:17:27 +0200

    crear los modelos en angular

---
+++ WEB/src/app/models/user.ts
@@ -0,0 +1,8 @@
+﻿export class User {
+    id!: string | null;
+    username!: string | null;
+    password!: string | null;
+    firstName!: string | null;
+    lastName!: string | null;
+    token!: string | null;
+}
