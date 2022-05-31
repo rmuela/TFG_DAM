dcfe569d43f8b1ed4223f7f068d9648d3a412bec
Rafael Muela <rafael316muela@gmail.com>
2022-06-01 00:20:58 +0200

    formulario de crear boda

---
+++ WEB/src/app/pages/create-wedding/create-wedding-component/create-wedding-component.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreateWeddingComponentComponent } from './create-wedding-component.component';
+
+describe('CreateWeddingComponentComponent', () => {
+  let component: CreateWeddingComponentComponent;
+  let fixture: ComponentFixture<CreateWeddingComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CreateWeddingComponentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreateWeddingComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ WEB/src/app/pages/create-wedding/create-wedding-component/create-wedding-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-wedding-component',
+  templateUrl: './create-wedding-component.component.html',
+  styleUrls: ['./create-wedding-component.component.css']
+})
+export class CreateWeddingComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
